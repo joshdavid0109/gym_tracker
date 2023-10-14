a04e2608d79ca915842596e0f41ae449594f75f2
JAHN CRYSTAN ABELLA <2224514@slu.edu.ph>
2023-10-14 17:59:26 +0800

    added validation of forms

--- AddClients/index.js
+++ AddClients/index.js
@@ -1,3 +1,4 @@
+// const to make sure shit wont change #pranning
 const form = document.getElementById("input-form");
 const fieldsets = form.querySelectorAll("fieldset");
 const nextButtons = form.querySelectorAll(".next");
@@ -17,6 +18,7 @@ function hideFieldset(index) {
     fieldsets[index].style.display = "none";
 }
 
+// func for the next button
 function nextFieldset() {
     if (currentFieldsetIndex < fieldsets.length - 1) {
         hideFieldset(currentFieldsetIndex);
@@ -26,6 +28,7 @@ function nextFieldset() {
     }
 }
 
+// func for the back button
 function previousFieldset() {
     if (currentFieldsetIndex > 0) {
         hideFieldset(currentFieldsetIndex);
@@ -36,11 +39,40 @@ function previousFieldset() {
 }
 
 
-nextButtons.forEach((button, index) => {
-    button.addEventListener("click", nextFieldset);
-});
+// func to validate the form
+function validateForm() {
+    const currentFieldset = fieldsets[currentFieldsetIndex];
+    const inputs = currentFieldset.querySelectorAll('input, select');
 
+    for (const input of inputs) {
+        if (input.hasAttribute('required') && input.value.trim() === '') {
+            alert('Please fill in all required fields.');
+            return false;
+        }
+    }
+
+    return true;
+}
+
+nextButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        // check if current form is valid
+        if (validateForm()) {
+            if (currentFieldsetIndex < fieldsets.length - 1) {
+                nextFieldset();
+            }
+        }
+    });
+});
 
 backButtons.forEach((button, index) => {
     button.addEventListener("click", previousFieldset);
+});
+
+
+submitButton.addEventListener("click", function () {
+    // if form is valid, user can submit the form 
+    if (validateForm()) {
+
+    }
 });
